1922639e602bac27157d0686b6fd381dfc85be27
Semestr Bali <user@semestr-bali.com>
2025-12-03 18:51:11 +0400

    Remove light/dark theme switching feature - revert to original single light theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
 import Calculator from './components/Calculator'
 import LanguageSwitcher from './components/LanguageSwitcher'
-import ThemeToggle from './components/ThemeToggle'
 import Footer from './components/Footer'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import TermsOfService from './pages/TermsOfService'
@@ -14,28 +12,25 @@ import './App.css'
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <div className="app-wrapper">
-          <Navbar />
-          <div className="language-switcher-container">
-            <LanguageSwitcher />
-          </div>
-          <ThemeToggle />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Calculator />} />
-              <Route path="/privacy" element={<PrivacyPolicy />} />
-              <Route path="/terms" element={<TermsOfService />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/copyright" element={<Copyright />} />
-            </Routes>
-          </div>
-          <Footer />
+    <Router>
+      <div className="app-wrapper">
+        <Navbar />
+        <div className="language-switcher-container">
+          <LanguageSwitcher />
         </div>
-      </Router>
-    </ThemeProvider>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Calculator />} />
+            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/terms" element={<TermsOfService />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/copyright" element={<Copyright />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
